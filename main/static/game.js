11d8ae057a4e2a92271ccb01c5df3aa4e7b66c62
Maciej Bąk <bak.maciej@protonmail.com>
2023-05-27 12:35:27 +0200

    add leaflet map

---
+++ main/static/game.js
@@ -0,0 +1,17 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
