619f0937eff32b845ae707480cf359602b256bdb
Maciej Bąk <bak.maciej@protonmail.com>
2023-05-27 22:50:49 +0200

    add tmp game score form

--- main/forms.py
+++ main/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from main.models import Game
+
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,3 +11,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+
+class GameForm(forms.ModelForm):
+    class Meta:
+        model = Game
+        fields = ['score']
